59daff28eab1ef246dbc78babd6f3ee940422f1b
Matt Sephton <matt@gingerbeardman.com>
2020-09-23 21:28:21 +0100

    changes for udisksctl and more recent rupor-minimal.7z

--- common.sh
+++ common.sh
@@ -54,12 +54,12 @@ check_command() {
 
 detect_disk_reader() {
     for DEV in /dev/sd?; do
-        if udisks --show-info $DEV | grep PRS-XXX >/dev/null; then
+        if udisksctl info -b $DEV | grep PRS-XXX >/dev/null; then
             abort "Reader found but in recovery mode. Reboot in normal mode and retry."
         fi
         
-        if udisks --show-info $DEV | egrep 'model:\s+PRS-T2$' >/dev/null; then
-            if udisks --show-info $DEV | egrep 'label:\s+READER$' >/dev/null; then
+        if udisksctl info -b $DEV | egrep 'drives/Sony_PRS_T2_' >/dev/null; then
+            if udisksctl info -b $DEV | egrep 'IdLabel:\s+READER$' >/dev/null; then
                 echo $DEV
                 return 0
             fi
@@ -69,8 +69,8 @@ detect_disk_reader() {
 
 detect_disk_sd() {
     for DEV in /dev/sd??; do
-        if udisks --show-info $DEV | grep 'by-id/usb-Sony_PRS-T2_SD' >/dev/null; then
-            if udisks --show-info $DEV | grep 'partition:$' >/dev/null; then
+        if udisksctl info -b $DEV | grep 'by-id/usb-Sony_PRS-T2_SD' >/dev/null; then
+            if udisksctl info -b $DEV | grep 'Partition:$' >/dev/null; then
                 echo $DEV
                 return 0
             fi
@@ -81,25 +81,26 @@ detect_disk_sd() {
 
 prs_mount_reader() {
     DEV=$(detect_disk_reader)
-    udisks --mount $DEV >/dev/null
-    udisks --show-info $DEV | grep 'mount paths:' | tr -d ' ' | cut -d: -f2
+    udisksctl mount -b $DEV >/dev/null
+    udisksctl info -b $DEV | grep 'MountPoints:' | tr -d ' ' | cut -d: -f2
 }
 
 prs_unmount_reader() {
     sync
     DEV=$(detect_disk_reader)
-    udisks --unmount $DEV
+    udisksctl unmount -b $DEV
 }
 
 prs_mount_sd() {
     DEV=$(detect_disk_sd) || return 1
 
-    udisks --mount $DEV >/dev/null || return 1
-    udisks --show-info $DEV | grep 'mount paths:' | tr -d ' ' | cut -d: -f2
+    udisksctl unmount -b $DEV >/dev/null
+    udisksctl mount -b $DEV >/dev/null || return 1
+    udisksctl info -b $DEV | grep 'MountPoints:' | tr -d ' ' | cut -d: -f2
 }
 
 prs_unmount_sd() {
     sync
     DEV=$(detect_disk_sd) || return 1
-    udisks --unmount $DEV
+    udisksctl unmount -b $DEV
 }
